4ba2b0219e38b01e86de68d904107618dbf38a1d
Ayush Gupta <ayushgupta20011@gmail.com>
2021-04-15 13:51:47 -0400

    add comments that states functionality of a function

--- pulp-client/db/db.py
+++ pulp-client/db/db.py
@@ -3,7 +3,9 @@ import sqlite3
 import click
 from flask import current_app, g
 from flask.cli import with_appcontext
+import os
 
+#get_db starts as well as connects to sqlite db(named as pulp) via a socket. Also creates a db named "pulp".
 def get_db():
     if "db" not in g:
         g.db = sqlite3.connect(
@@ -13,17 +15,24 @@ def get_db():
 
     return g.db
 
+
+# close_db closes a database connection if its already running.
 def close_db(e=None):
     db = g.pop("db", None)
 
     if db is not None:
         db.close()
 
+# init_db basically creates schema.sql into a table.
+# "with current_app.open_resource("db/schema.sql") as f" basically automatically calls f.close after its body's execution.
 def init_db():
-    db = get_db()
+    db = sqlite3.connect(
+            "pulp", detect_types=sqlite3.PARSE_DECLTYPES
+        )
+    db.row_factory = sqlite3.Row
 
-    with current_app.open_resource("db/schema.sql") as f:
-        db.executescript(f.read().decode("utf8"))
+    with open("db/schema.sql") as f:
+        db.executescript(f.read())
 
 @click.command("init-db")
 @with_appcontext
@@ -32,6 +41,11 @@ def init_db_command():
     init_db()
     click.echo("Initialized the database.")
 
+#teardown closes database
+#add_command calls init db which creates and connects to the db.
 def init_app(app):
     app.teardown_appcontext(close_db)
     app.cli.add_command(init_db_command)
+
+if __name__ == '__main__':
+    init_db()
