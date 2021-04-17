4bef1de1fed17cf78faa02192ce412d0bb38c779
Ayush Gupta <ayushgupta20011@gmail.com>
2021-04-17 02:21:51 -0400

    now it redirects to dashboard/user/userid

--- pulp-client/handlers/homepage.py
+++ pulp-client/handlers/homepage.py
@@ -1,20 +1,15 @@
 from flask_login import current_user
 from flask import Blueprint
+from flask import redirect
 
 homepage = Blueprint('homepage', __name__, template_folder='templates')
 
+
+#when user is authenticated, it'll show addressbox, if not it'll redirect to login page.
 @homepage.route('/')
 def index():
-    if current_user.is_authenticated:
-        return (
-            "<p>Hello, {}! You're logged in! Email: {} Address: {}</p>"
-            "<div><p>Google Profile Picture:</p>"
-            '<img src="{}" alt="Google profile pic"></img></div>'
-            '<a class="button" href="/logout">Logout</a>'.format(
-                current_user.name, current_user.email, current_user.address, current_user.profile_pic
-            )
-        )
-    else:
+    if not current_user.is_authenticated:
+        
         return '''<div class="col s12 m6 offset-m3 center-align">
     <a class="oauth-container btn darken-4 white black-text" href="/login" style="text-transform:none">
         <div class="left">
@@ -31,3 +26,7 @@ def index():
 <!-- Compiled and minified JavaScript -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script>
 '''
+    else :
+        return redirect("/dashbord/users/" + current_user.id)
+
+
