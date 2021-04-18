29ef9d071193265b1a5fdad9921e53510c50e7d4
Ayush Gupta <ayushgupta20011@gmail.com>
2021-04-18 03:29:54 -0400

    added table for orders and shop

--- pulp-client/db/schema.sql
+++ pulp-client/db/schema.sql
@@ -1,7 +1,31 @@
-CREATE TABLE user (
+CREATE TABLE IF NOT EXISTS user (
   id TEXT PRIMARY KEY,
   name TEXT NOT NULL,
   email TEXT UNIQUE NOT NULL,
   profile_pic TEXT NOT NULL,
+  -- orderid INT UNIQUE NOT NULL,
   address TEXT NULL
+  -- FOREIGN KEY (orderid) REFRENCES order (orderid)
 );
+
+
+CREATE TABLE IF NOT EXISTS shop (
+  shopid INT UNIQUE NOT NULL,
+  shop_name TEXT NOT NULL,
+  shop_email TEXT NOT NULL,
+  shop_address TEXT NOT NULL,
+  PRIMARY KEY (shopid)
+);
+
+
+CREATE TABLE orders (
+	orderid INT AUTO_INCREMENT,
+	file BLOB NOT NULL,
+	shopid INT NOT NULL,
+	status TEXT, 
+	userid TEXT NOT NULL,
+	PRIMARY KEY (orderid),
+	FOREIGN KEY (shopid) REFERENCES shop (shopid),
+	FOREIGN KEY (userid) REFERENCES user (id)
+);
+
