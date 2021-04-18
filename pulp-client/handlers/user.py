dc25ad8d87d7bc542955bdb5a54d4294d3dcc034
Ayush Gupta <ayushgupta20011@gmail.com>
2021-04-18 12:11:59 -0400

    now there iss a logout button on every page

--- pulp-client/handlers/user.py
+++ pulp-client/handlers/user.py
@@ -14,12 +14,12 @@ userpage = Blueprint('userpage', __name__, template_folder='templates')
 def get(userid):
     #here the user is verified and we have his address as well
     if current_user.is_authenticated and current_user.address :
-        return (
+        return ("<center>"
             "<p>Hello, {}! You're logged in! Email: {} Address: {}</p>"
             "<div><p>Google Profile Picture:</p>"
             '<img src="{}" alt="Google profile pic"></img></div>'
             '<a class="button" href="/logout">Logout</a>'
-            '''<p><button onclick="window.location.href='/dashboard/users/{}/orders'">Show Orders</button></p>'''.format(
+            '''<p><button onclick="window.location.href='/dashboard/users/{}/orders'">Show Orders</button></p></center>'''.format(
                 current_user.name, current_user.email, current_user.address, current_user.profile_pic, current_user.id
             
             )
@@ -28,11 +28,12 @@ def get(userid):
     elif current_user.is_authenticated :
         return (
             '''
-            <form action="/dashboard/users/{}" method="post">
+            <center><form action="/dashboard/users/{}" method="post">
             <label for="address">Please Add your address to continue:</label><br>
             <input type="text" id="address" name="address"><br>
             <input type="submit" value="Submit">
-            </form> '''.format(current_user.id) 
+            <p><a class="button" href="/logout">Logout</a></p>
+            </form></center> '''.format(current_user.id) 
         )
 
     #here if we have nither authenticated user or address (cant have address if you dont have authenticated user)
